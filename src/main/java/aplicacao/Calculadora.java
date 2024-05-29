8f19d80021f5715be5206da9b50ab6ed3837476d
Alexandre de Souza Flores <133156808+alesouzaflores@users.noreply.github.com>
2024-05-28 21:37:35 -0300

    Update Calculadora.java

--- src/main/java/aplicacao/Calculadora.java
+++ src/main/java/aplicacao/Calculadora.java
@@ -3,7 +3,7 @@ package aplicacao;
 public class Calculadora {
 
 	public double soma(double n1, double n2) {
-		return n1 + n2;
+		return n1 - n2;
 	}
 	
 	public double subtracao(double n1, double n2) {
